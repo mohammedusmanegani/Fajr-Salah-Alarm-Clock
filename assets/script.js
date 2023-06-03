afa8a87b11da14e946b55619c49d78611632affd
Mohammed Usman E Gani <ka.usmanegani@gmail.com>
2023-06-03 22:37:21 +0530

    Can Add Future Times

--- assets/script.js
+++ assets/script.js
@@ -22,98 +22,93 @@ function setAlarm() {
   const resetAlarmBtn = document.getElementById('reset-alarm-btn')
   const passwordForm = document.getElementById('password-form')
 
-  const [alarmTimeHours, alarmTimeMinutes] = alarmTime.split(':')
-  const alarmTimeMs = new Date().setHours(
-    alarmTimeHours,
-    alarmTimeMinutes,
-    0,
-    0,
-  )
-  const currentTimeMs = new Date().getTime()
-  const timeLeftMs = alarmTimeMs - currentTimeMs
+  const now = new Date()
+  const selectedTime = new Date(now.toDateString() + ' ' + alarmTime)
 
-  if (timeLeftMs > 0) {
-    const daysLeft = Math.floor(timeLeftMs / (1000 * 60 * 60 * 24))
-    const hoursLeft = Math.floor(
-      (timeLeftMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-    )
-    const minutesLeft = Math.floor(
-      (timeLeftMs % (1000 * 60 * 60)) / (1000 * 60),
-    )
-    const secondsLeft = Math.floor((timeLeftMs % (1000 * 60)) / 1000)
-
-    const timeLeftText = 'Time left for Fajr Salah: '
-
-    const timeLeftString =
-      daysLeft.toString().padStart(2, '0') +
-      'd:' +
-      hoursLeft.toString().padStart(2, '0') +
-      'h:' +
-      minutesLeft.toString().padStart(2, '0') +
-      'm:' +
-      secondsLeft.toString().padStart(2, '0') +
-      's'
-    timeLeftElement.textContent = timeLeftText + timeLeftString
-
-    const countdown = setInterval(() => {
-      const currentTimeMs = new Date().getTime()
-      const timeLeftMs = alarmTimeMs - currentTimeMs
-
-      if (timeLeftMs <= 0) {
-        clearInterval(countdown)
-        timeLeftElement.textContent = ''
-        alarmModal.classList.remove('hidden')
-        alarmSound.play()
-      } else {
-        const daysLeft = Math.floor(timeLeftMs / (1000 * 60 * 60 * 24))
-        const hoursLeft = Math.floor(
-          (timeLeftMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-        )
-        const minutesLeft = Math.floor(
-          (timeLeftMs % (1000 * 60 * 60)) / (1000 * 60),
-        )
-        const secondsLeft = Math.floor((timeLeftMs % (1000 * 60)) / 1000)
-
-        const timeLeftString =
-          daysLeft.toString().padStart(2, '0') +
-          'd:' +
-          hoursLeft.toString().padStart(2, '0') +
-          'h:' +
-          minutesLeft.toString().padStart(2, '0') +
-          'm:' +
-          secondsLeft.toString().padStart(2, '0') +
-          's'
-        timeLeftElement.textContent = timeLeftText + timeLeftString
-      }
-    }, 1000)
-
-    stopAlarmBtn.addEventListener('click', () => {
-      passwordForm.classList.remove('hidden')
-    })
-
-    document
-      .getElementById('submit-password-btn')
-      .addEventListener('click', () => {
-        const passwordInput = document.getElementById('password').value
-        if (passwordInput === '1234') {
-          alarmSound.pause()
-          alarmSound.currentTime = 0
-          alarmModal.classList.add('hidden')
-          resetAlarmBtn.classList.remove('hidden')
-          passwordForm.classList.add('hidden')
-        } else {
-          alert('Incorrect Password. Alarm is still ringing!')
-        }
-      })
-
-    document.getElementById('reset-alarm-btn').addEventListener('click', () => {
-      resetAlarmBtn.classList.add('hidden')
-      document.getElementById('alarm-time').value = ''
-      timeLeftElement.textContent = ''
-    })
-  } else {
-    alert('Please select a future time for the alarm!')
+  // Check if the selected time is in the past
+  if (selectedTime.getTime() < now.getTime()) {
+    // Add one day to the selected time
+    selectedTime.setDate(selectedTime.getDate() + 1)
   }
+
+  const timeLeftMs = selectedTime.getTime() - now.getTime()
+
+  const daysLeft = Math.floor(timeLeftMs / (1000 * 60 * 60 * 24))
+  const hoursLeft = Math.floor(
+    (timeLeftMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  )
+  const minutesLeft = Math.floor((timeLeftMs % (1000 * 60 * 60)) / (1000 * 60))
+  const secondsLeft = Math.floor((timeLeftMs % (1000 * 60)) / 1000)
+
+  const timeLeftText = 'Time left: '
+
+  const timeLeftString =
+    daysLeft.toString().padStart(2, '0') +
+    'd:' +
+    hoursLeft.toString().padStart(2, '0') +
+    'h:' +
+    minutesLeft.toString().padStart(2, '0') +
+    'm:' +
+    secondsLeft.toString().padStart(2, '0') +
+    's'
+  timeLeftElement.textContent = timeLeftText + timeLeftString
+
+  const countdown = setInterval(() => {
+    const now = new Date()
+    const timeLeftMs = selectedTime.getTime() - now.getTime()
+
+    if (timeLeftMs <= 0) {
+      clearInterval(countdown)
+      timeLeftElement.textContent = ''
+      alarmModal.classList.remove('hidden')
+      alarmSound.play()
+    } else {
+      const daysLeft = Math.floor(timeLeftMs / (1000 * 60 * 60 * 24))
+      const hoursLeft = Math.floor(
+        (timeLeftMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+      )
+      const minutesLeft = Math.floor(
+        (timeLeftMs % (1000 * 60 * 60)) / (1000 * 60),
+      )
+      const secondsLeft = Math.floor((timeLeftMs % (1000 * 60)) / 1000)
+
+      const timeLeftString =
+        daysLeft.toString().padStart(2, '0') +
+        'd:' +
+        hoursLeft.toString().padStart(2, '0') +
+        'h:' +
+        minutesLeft.toString().padStart(2, '0') +
+        'm:' +
+        secondsLeft.toString().padStart(2, '0') +
+        's'
+      timeLeftElement.textContent = timeLeftText + timeLeftString
+    }
+  }, 1000)
+
+  stopAlarmBtn.addEventListener('click', () => {
+    passwordForm.classList.remove('hidden')
+  })
+
+  document
+    .getElementById('submit-password-btn')
+    .addEventListener('click', () => {
+      const passwordInput = document.getElementById('password').value
+      if (passwordInput === '1234') {
+        alarmSound.pause()
+        alarmSound.currentTime = 0
+        alarmModal.classList.add('hidden')
+        resetAlarmBtn.classList.remove('hidden')
+        passwordForm.classList.add('hidden')
+      } else {
+        alert('Incorrect Password. Alarm is still ringing!')
+      }
+    })
+
+  document.getElementById('reset-alarm-btn').addEventListener('click', () => {
+    resetAlarmBtn.classList.add('hidden')
+    document.getElementById('alarm-time').value = ''
+    timeLeftElement.textContent = ''
+  })
 }
 
 // Initialize the alarm clock
